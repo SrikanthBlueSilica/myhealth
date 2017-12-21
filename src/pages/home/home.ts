cfedae39a1e19315949bef83f6ce4172ea945d7a
srikanth <grvnsrinivas@gmail.com>
2017-12-21 17:09:57 +0530

    ionic login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 
 export class HomePage {
+  public name:any;
+  public password:any;
+  public log:Array<any>=[];
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  login(){
+    var log={
+      name:this.name,
+      password:this.password
+    }
+    this.log.push(log);
+    console.log('saved',this.log);
+  }
 }
