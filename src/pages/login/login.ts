3e9acedef8731a6c7b339dc8287fa279fca39076
srikanth <grvnsrinivas@gmail.com>
2017-12-26 18:02:14 +0530

    ionic save

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Events } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 @Component({
   templateUrl: 'login.html'
@@ -7,6 +9,26 @@ import { NavController } from 'ionic-angular';
 
 export class LoginPage {
 
-  constructor() {}
+  public username:any;
+  public password:any;
+
+
+  constructor(public navCtrl: NavController,public events: Events) {}
+
+  changePage(event) {
+    // Sharing data using NavController
+    this.navCtrl.push(HomePage, {
+      
   
+    });
+  } 
+
+  login() {
+    var log = document.getElementById("cheking");
+    if(this.username==''||this.username==null||this.password==''||this.password==null) {
+      log.setAttribute("disabled","disabled");
+    } else  {
+      log.removeAttribute("disabled");
+    }
+  }
 }
